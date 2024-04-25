2bf87f3ea35f0b07f31bf1686841f0896f3b0018
arthursargsian <arthur.sargian2.g@gmail.com>
2024-04-25 17:46:19 +0400

    Completed favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+    import {Html, Head, Main, NextScript} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+
+    export default function Document() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="shortcut icon" href="/image/favicon.jpeg"/>
+                    <title>Portfolio</title>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
